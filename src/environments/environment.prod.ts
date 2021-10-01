c67e37f6d98e84b81141d4f6baa8ce93266a0bac
Markov Kirill <Ker1n.mkc@gmail.com>
2021-10-01 12:10:31 +0300

    switch to angular 9

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,6 @@ import {Environment} from './interface';
 
 export const environment: Environment = {
   production: true,
-  apiKey: 'AIzaSyD1gBONbhOwYeqLSix3em7QMa0xeafB8kU'
+  apiKey: 'AIzaSyD1gBONbhOwYeqLSix3em7QMa0xeafB8kU',
+  fbDbUrl: 'https://angular-blog-61e21-default-rtdb.asia-southeast1.firebasedatabase.app/'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,7 +6,8 @@ import {Environment} from './interface';
 
 export const environment: Environment = {
   production: false,
-  apiKey: 'AIzaSyD1gBONbhOwYeqLSix3em7QMa0xeafB8kU'
+  apiKey: 'AIzaSyD1gBONbhOwYeqLSix3em7QMa0xeafB8kU',
+  fbDbUrl: 'https://angular-blog-61e21-default-rtdb.asia-southeast1.firebasedatabase.app/'
 };
 
 /*
