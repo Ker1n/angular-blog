470d17ee064c71c9d3c67d5cffd7b4d184d87226
Markov Kirill <Ker1n.mkc@gmail.com>
2021-10-01 10:49:30 +0300

    init & admin page and routing

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
-export const environment = {
-  production: true
+import {Environment} from './interface';
+
+export const environment: Environment = {
+  production: true,
+  apiKey: 'AIzaSyD1gBONbhOwYeqLSix3em7QMa0xeafB8kU'
 };
